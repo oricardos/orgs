81035fa54c9afeb4f9ddf9af267d4bd98dc4abd8
oricardos <ricardo.rs95@gmail.com>
2023-05-30 23:42:51 -0300

    feat(geral): add AppLoading

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { SafeAreaView, View, StatusBar } from 'react-native';
 import { Basket } from './src/screens/Basket';
 import { useFonts, Rubik_400Regular, Rubik_600SemiBold, Rubik_700Bold } from '@expo-google-fonts/rubik';
+import AppLoading from 'expo-app-loading';
 import mock from './src/mocks/cesta'
 
 export default function App() {
@@ -11,7 +12,7 @@ export default function App() {
   });
 
   if (!fontLoaded) {
-    return <View />
+    return <AppLoading />
   }
 
   return (
